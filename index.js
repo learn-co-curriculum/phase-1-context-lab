432e30d4cba5379a3b6c5ba279872e66a92a7120
Liz Burton <liz.burton147@gmail.com>
2021-06-30 10:45:54 -0400

    Update starter code to use const

--- index.js
+++ index.js
@@ -9,14 +9,15 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
+const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
+    const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
 }
+
